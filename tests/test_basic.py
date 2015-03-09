96ce4374dcc3259284653bb17760536e881159b0
Ian Denhardt <ian@zenhack.net>
2015-03-08 20:21:33 -0400

    Clean up the test suite a bit.
    * Add comments
    * Add a test for an uncommon use case
    * Swap out P for a custom thing imported from untemplate.elements in one
      of the tests. This, combined with a comment, makes it a little more
      clear what's going on.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,24 +1,40 @@
 from untemplate.html import *
 from untemplate.node import Element, Markup
+from untemplate.elements import ExampleElt
 from untemplate_testing import same_doc
 import pytest
 
+# Let's not forget the trivial stuff; test Element.__repr__
 assert repr(A(href="/")("Home")) == "<Element('a',{'href': '/'},('Home',))>"
 
+# Checks for correct output
+
+## Trivial case
 assert same_doc(Element('p'), "<p/>")
 
+## Check that text is escaped properly
 assert same_doc(Element('p')("Hello, <world/>"),
                 "<p>Hello, &lt;world/&gt;</p>")
 
+## Make sure the element auto-import mechanism is working correctly.
+assert same_doc(Element('exampleelt'), str(ExampleElt))
 
-assert same_doc(Element('p'), str(P))
 
+## Make sure `Markup` prevents
 assert same_doc(P(Markup('<span>Hello</span>')),
                 '<p><span>Hello</span></p>')
 
+## Test something with attributes.
 assert same_doc(A(href="/index.html")("Home"),
                 '<a href="/index.html">Home</a>')
 
+## This isn't something you'll see often because it looks awkward, but
+## it should work: passing both contents and attributes in the same call.
+assert same_doc(A("Home", href="/index.html"),
+                '<a href="/index.html">Home</a>')
+
+
+# Checks for invalid input from the user
 
 class WriterToTestObj(object):
     """Utility class for exercising "type checking."
